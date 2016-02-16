a27457ad62a22610cf1b65240b878c3ee643bfd4
dm.delgado10 <dm.delgado10@uniandes.edu.co>
2016-02-16 08:54:00 -0500

    nuevos modulos, diferentes ui views

--- unitrip/src/main/webapp/src/app.js
+++ unitrip/src/main/webapp/src/app.js
@@ -7,11 +7,21 @@
         }]);
 
     mod.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
-            $urlRouterProvider.otherwise("/viajes");
+            $urlRouterProvider.otherwise("/inicio");
             $stateProvider
+                    
+                    .state ('inicio', {
+                        url: '/inicio',
+                        templateUrl: "src/modules/inicio/inicio.tpl.html"
+                    })
                     .state('config', {
                         url: '/config',
                         templateUrl: "src/modules/cuenta/config.tpl.html"
+                    })
+            
+                    .state('experiencias', {
+                    url: '/experiencias',
+                    templateUrl: "src/modules/experiencias/experiencias.tpl.html"
                     })
                     .state('viajes', {
                     url: '/viajes',
