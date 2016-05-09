2ea098d1a84c70f70833e77a4b9d540adf8b2c34
je.molano1498 <je.molano1498@172.24.100.221>
2016-05-09 18:00:53 -0500

    Modificacion pequenia

--- unitrip-web/src/main/java/co/edu/uniandes/rest/resources/ViajesResource.java
+++ unitrip-web/src/main/java/co/edu/uniandes/rest/resources/ViajesResource.java
@@ -194,7 +194,7 @@ public class ViajesResource {
      */
     @DELETE
     @Path("{viajeId: \\d+}")
-    public void removeViaje(@PathParam("viajeroId") Long viajeroId, @PathParam("viajeId") Long viajeId) {
+    public void removeViaje(@PathParam("viajeroId") Long viajeroId, @PathParam("viajeId") Long viajeId) throws BusinesLogicException {
         viajeroLogic.removeViaje(viajeroId, viajeId);
 
     }
